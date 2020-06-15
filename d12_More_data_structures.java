a1df0436465bc767ad0f2ac0b1f3fda9dc226b6a
David Adams <david.riding.rails@gmail.com>
2020-06-15 11:11:37 -0700

    add more examples and comments

--- d12_More_data_structures.java
+++ d12_More_data_structures.java
@@ -50,7 +50,7 @@ class Main {
 
     System.out.println(animals);
 
-    System.out.println("A toad in the linked list? " + animals.contains("toad"));
+    System.out.println("Is a toad in the linked list? " + animals.contains("toad"));
 
     Integer howMany = animals.size();
     System.out.printf("My LinkedList of animals has %d entries\n", howMany);
@@ -70,11 +70,12 @@ class Main {
 
   public static void main(String[] args) {
     Queue<Integer> bbq = new LinkedList<Integer>();
+    // Queue<Integer> badQ = new Queue<Integer>(); // THIS WON'T WORK!!!
     for(int i = 1; i < 7; i++) bbq.add(i);
 
     System.out.println(bbq);
 
-    Integer hat = bbq.remove();
+    Integer hat = bbq.remove(); // it's 'hat' becaue it's the head element...
     System.out.printf("removed head element: %d\n", hat);
 
     System.out.println(bbq);
@@ -92,29 +93,78 @@ class Main {
 
 // Stack
 // last in first out
+import java.util.Stack;
 
 class Main {
 
   public static void main(String[] args) {
-    Stack<String> strStack = new Stack<String>();
+    Stack<String> fruitStack = new Stack<String>();
+
+    System.out.println(fruitStack.capacity());
+
+    for(Integer i = 0; i < 15; i++){
+      fruitStack.push(i.toString());
+    }
+
+    System.out.println(fruitStack.capacity());
+
+    fruitStack.push("apple");
+    fruitStack.push("banana");
+    fruitStack.push("cherry");
+
+    System.out.println(fruitStack.capacity());
+
+    System.out.println(fruitStack);
+
+    System.out.println(fruitStack.peek());
+
+    System.out.println(fruitStack.empty());
     
-    strStack.push("apple");
-    strStack.push("banana");
-    strStack.push("cherry");
-
-    System.out.println(strStack);
-
-    System.out.println(strStack.peek());
-
-    System.out.println(strStack.empty());
-
-    System.out.println(strStack);
-
     System.out.println(strStack.pop());
 
     System.out.println(strStack);
 
-    System.out.println(strStack.search("banana"));
+    System.out.println(fruitStack.search("banana"));
+
+  }
+}
+
+
+//Some extra examples with arrays
+//try leaving out the import to see the error message
+import java.util.Arrays;
+
+class Main {
+
+  public static void main(String[] args) {
+
+    int[] numbers = {3, 5, 7, 44, 65, 23};
+
+    System.out.println(Arrays.toString(numbers)); 
+    //look in the inheritance list on Oracle documentation 
+    //to find where it gets the .toString() method for Arrays, 
+    //click the link to read about usage and pay attention to the source
+
+    int sum = 0;
+
+    for(int thingy : numbers){
+      sum += thingy;
+    }
+
+    System.out.println(sum);
+
+    // for this try to think it through and predict what the result will be
+    long[] exampleArray = {1, 5, 6, 5, 4, 1};
+    long maximum = exampleArray[0];
+    int index = 0;
+    for(int i = 1; i<exampleArray.length; i++){
+      if (exampleArray[i] > maximum) {
+        maximum = exampleArray[i];
+        index = i;
+      }
+    }
+    System.out.println(index);
+
   }
 }
 
